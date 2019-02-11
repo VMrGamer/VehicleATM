f111de162485702fc34d0fc7b72927e52875694e
VMrGamer <v.mr.gamer@gmail.com>
2019-02-11 14:37:02 +0530

    Splash Screen to Login complete

--- app/src/main/java/cybersociety/vehicleatm/SplashScreenActivity.java
+++ app/src/main/java/cybersociety/vehicleatm/SplashScreenActivity.java
@@ -1,13 +1,54 @@
 package cybersociety.vehicleatm;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d("@@@@", "splash:signed_in:" + user.getUid());
+                    Intent home = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(home);
+                    finish();
+                } else {
+                    Log.d("@@@@", "splash:signed_out");
+                    Intent login = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                    startActivity(login);
+                    finish();
+                }
+            }
+        };
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
