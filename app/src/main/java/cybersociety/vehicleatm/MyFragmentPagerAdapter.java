ef432977ab835fbbedc5a6a5a62197096aab77b7
VMrGamer <v.mr.gamer@gmail.com>
2019-02-14 09:16:01 +0530

    Fixed TabLayout

--- app/src/main/java/cybersociety/vehicleatm/MyFragmentPagerAdapter.java
+++ app/src/main/java/cybersociety/vehicleatm/MyFragmentPagerAdapter.java
@@ -1,18 +1,28 @@
 package cybersociety.vehicleatm;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+    private String[] tabTitles = new String[]{"All", "Register", "History"};
+
     public MyFragmentPagerAdapter(FragmentManager fm){
         super(fm);
     }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+
     public Fragment getItem(int position){
         Fragment fragment =null;
         switch (position){
             case 0:
-                fragment=new fragmentAll();
+                fragment = fragmentAll.newInstance(0, "All");
                 break;
             case 1:
                 fragment=new fragmentRegister();
--- app/src/main/java/cybersociety/vehicleatm/fragmentAll.java
+++ app/src/main/java/cybersociety/vehicleatm/fragmentAll.java
@@ -7,11 +7,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class fragmentAll extends Fragment {
+
+    private String title;
+    private int page;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_all,null);
+        View view = inflater.inflate(R.layout.fragment_all, container, false);
+        return view;
     }
+
+    public static fragmentAll newInstance(int page, String title) {
+        fragmentAll fragmentall = new fragmentAll();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragmentall.setArguments(args);
+        return fragmentall;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+    }
+
 }
