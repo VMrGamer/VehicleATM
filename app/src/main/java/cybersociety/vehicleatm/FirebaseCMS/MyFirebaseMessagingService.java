e29669356c5db33c6609b0904380ba2933f3129b
VMrGamer <v.mr.gamer@gmail.com>
2019-02-18 10:40:39 +0530

    Changes in Notification

--- app/src/main/java/cybersociety/vehicleatm/FirebaseCMS/MyFirebaseMessagingService.java
+++ app/src/main/java/cybersociety/vehicleatm/FirebaseCMS/MyFirebaseMessagingService.java
@@ -1,6 +1,5 @@
-package cybersociety.vehicleatm;
+package cybersociety.vehicleatm.FirebaseCMS;
 
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -10,7 +9,6 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
@@ -18,6 +16,9 @@ import com.firebase.jobdispatcher.GooglePlayDriver;
 import com.firebase.jobdispatcher.Job;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import cybersociety.vehicleatm.MainActivity;
+import cybersociety.vehicleatm.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -29,7 +30,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Log.d(TAG, "onMessageReceived: called");
         Log.d(TAG, "onMessageReceived: data: "+remoteMessage.getNotification().getBody());
         Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-        sendNotification(remoteMessage.getNotification().getBody());
+        sendNotification(remoteMessage);
     }
 
     @Override
@@ -70,6 +71,36 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                         .setContentText(messageBody)
                         .setAutoCancel(true)
                         .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 , notificationBuilder.build());
+    }
+    private void sendNotification(RemoteMessage remoteMessage) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                        .setContentTitle(remoteMessage.getNotification().getTitle())
+                        .setContentText(remoteMessage.getNotification().getBody())
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setColor(getResources().getColor(R.color.colorAccent, getTheme()))
                         .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager =
--- app/src/main/java/cybersociety/vehicleatm/FirebaseCMS/MyJobService.java
+++ app/src/main/java/cybersociety/vehicleatm/FirebaseCMS/MyJobService.java
@@ -1,4 +1,4 @@
-package cybersociety.vehicleatm;
+package cybersociety.vehicleatm.FirebaseCMS;
 
 import android.util.Log;
 
