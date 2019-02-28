3e492ca81db4f6d3d5e1604139d65c17ca8ec93c
anand <16103497@nuv.ac.in>
2019-02-28 10:39:54 +0530

    guest registration is done.

---
+++ app/src/main/java/cybersociety/vehicleatm/GuestReg.java
@@ -0,0 +1,71 @@
+package cybersociety.vehicleatm;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class GuestReg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_reg);
+
+        final EditText name, contact_no, desc;
+        name = findViewById(R.id.editText);
+        contact_no = findViewById(R.id.editText4);
+        desc = findViewById(R.id.editText3);
+        Button b1 = findViewById(R.id.button2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user!=null){
+                    Log.d("TAG", "proceeding reg..");
+                    Toast.makeText(getApplicationContext(), "PROCEEDING REG...", Toast.LENGTH_LONG).show();
+                    Map<String, Object> doc_vehicle = new HashMap<>();
+                    doc_vehicle.put("guest name", name.getText().toString());
+                    doc_vehicle.put("contact no", contact_no.getText().toString());
+                   doc_vehicle.put("occasion", desc.getText().toString());
+                   Toast.makeText(getApplicationContext(), "DOC STORED...", Toast.LENGTH_LONG).show();
+                   FirebaseFirestore.getInstance().collection("users").document(user.getUid()).collection("guest").add(doc_vehicle).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d(TAG, "DocumentSnapshot successfully written!");
+                            Toast.makeText(getApplicationContext(), "WRITE SUCCESSFUL..", Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error writing document", e);
+                            Toast.makeText(getApplicationContext(), "WRITE UNSUCCESSFUL..", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "NULL USER", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
