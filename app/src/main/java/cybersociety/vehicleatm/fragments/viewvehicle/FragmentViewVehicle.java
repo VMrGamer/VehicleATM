d3a0f4120529fdfaec86c63ac891f14ea85c1cde
VMrGamer <v.mr.gamer@gmail.com>
2019-03-01 21:54:38 +0530

    Cleanup FragmentViewVehicle

--- app/src/main/java/cybersociety/vehicleatm/fragments/viewvehicle/FragmentViewVehicle.java
+++ app/src/main/java/cybersociety/vehicleatm/fragments/viewvehicle/FragmentViewVehicle.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 
 import android.os.Bundle;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 
 import android.support.v4.app.Fragment;
@@ -13,7 +14,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 import android.widget.Toast;
@@ -24,26 +24,8 @@ import android.view.ViewGroup;
 import cybersociety.vehicleatm.AppHelper;
 import cybersociety.vehicleatm.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FragmentViewVehicle.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FragmentViewVehicle#newInstance} factory method to
- * create an instance of this fragment.
- */
-
 
 public class FragmentViewVehicle extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -52,53 +34,26 @@ public class FragmentViewVehicle extends Fragment {
     // @BindView(R.id.recycler_view)
     // RecyclerView recyclerView;
 
-
     private ViewVehicleAdapter mAdapter;
 
     private ArrayList<ViewVehicleModel> modelList = new ArrayList<>();
 
-
     public FragmentViewVehicle() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentViewVehicle.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentViewVehicle newInstance(String param1, String param2) {
-        FragmentViewVehicle fragment = new FragmentViewVehicle();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     public static FragmentViewVehicle newInstance() {
-        FragmentViewVehicle fragment = new FragmentViewVehicle();
-        return fragment;
+        return new FragmentViewVehicle();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_view_vehicle, container, false);
@@ -112,7 +67,7 @@ public class FragmentViewVehicle extends Fragment {
 
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         setAdapter();
@@ -121,7 +76,7 @@ public class FragmentViewVehicle extends Fragment {
 
     private void findViews(View view) {
 
-        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        recyclerView = view.findViewById(R.id.recycler_view);
     }
 
 
@@ -167,7 +122,6 @@ public class FragmentViewVehicle extends Fragment {
 
 
     }
-
 
     /**
      * This interface must be implemented by activities that contain this
