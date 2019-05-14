89226eeabe08d2b7c9f98234987f2e88cdbe3c89
VMrGamer <v.mr.gamer@gmail.com>
2019-05-14 23:27:49 +0530

    Adding Contact Fragment for POC, Guard & Resident

---
+++ app/src/main/java/cybersociety/vehicleatm/fragments/contact/ContactModel.java
@@ -0,0 +1,36 @@
+package cybersociety.vehicleatm.fragments.contact;
+
+import java.util.ArrayList;
+
+public class ContactModel {
+
+    private String title;
+
+    private String message;
+
+
+    public ContactModel(String title, String message) {
+        this.title = title;
+        this.message = message;
+    }
+
+    public ContactModel() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
